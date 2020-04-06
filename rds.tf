5e89e59f2c5ee087c1ac4dfb26e6a04aa4b0d5ec
Joseph Roberts <jroberts@jroberts.me.uk>
2020-04-06 11:28:21 +0100

    Identifier

--- rds.tf
+++ rds.tf
@@ -1,12 +1,17 @@
 resource "aws_db_instance" "miami" {
+  identifier           = "miami"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
   engine_version       = "11.5"
   instance_class       = "db.t3.micro"
-  name                 = "miami"
+  name                 = "postgres"
   username             = "postgres"
   password             = "postgres"
   parameter_group_name = "default.postgres11"
   skip_final_snapshot  = true
+  
+  lifecycle {
+    ignore_changes = [password]
+  }
 }
