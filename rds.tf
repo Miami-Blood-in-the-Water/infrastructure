594c105b359e69d34f8b10c98eb5a0e5e9a88ff2
Joseph Roberts <jroberts@jroberts.me.uk>
2020-04-06 11:23:30 +0100

    identifier, not identiy

--- rds.tf
+++ rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "miami" {
-  identity             = "miami"
+  identifier           = "miami"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
