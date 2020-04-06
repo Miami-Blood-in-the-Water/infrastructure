8561159da4c3510a2c457064a55920ef11e35885
Joseph Roberts <jroberts@jroberts.me.uk>
2020-04-06 11:26:43 +0100

    Don't set identifier right now.

--- rds.tf
+++ rds.tf
@@ -1,11 +1,10 @@
 resource "aws_db_instance" "miami" {
-  identifier           = "miami"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
   engine_version       = "11.5"
   instance_class       = "db.t3.micro"
-  name                 = "postgres"
+  name                 = "miami"
   username             = "postgres"
   password             = "postgres"
   parameter_group_name = "default.postgres11"
