ff57a7cbfc767d6650b476ca20166ddc15380fd5
Joseph Roberts <jroberts@jroberts.me.uk>
2020-04-06 11:20:24 +0100

    Identifier, not name.

--- rds.tf
+++ rds.tf
@@ -1,10 +1,11 @@
 resource "aws_db_instance" "miami" {
+  identity             = "miami"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "postgres"
   engine_version       = "11.5"
   instance_class       = "db.t3.micro"
-  name                 = "miami"
+  name                 = "postgres"
   username             = "postgres"
   password             = "postgres"
   parameter_group_name = "default.postgres11"
